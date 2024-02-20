c0b7c00527f3a58c3533efc24c0c8c5244da682c
amirholya01 <amirholya01@gmail.com>
2024-02-20 09:13:44 +0100

    login

--- app/router/user/auth/auth.js
+++ app/router/user/auth/auth.js
@@ -1,12 +1,12 @@
 const {AuthController} = require("../../../controllers/user/auth/Auth.Controller");
 const {expressValidatorMapper} = require("../../../middleware/checkError");
-const {registerValidator} = require("../../../validation/user/auth");
+const {registerValidator, loginValidator} = require("../../../validation/user/auth");
 
 
 const router = require("express").Router();
 
 router.post("/register", registerValidator(), expressValidatorMapper, AuthController.register)
-
+router.post("/login", loginValidator(), expressValidatorMapper, AuthController.login);
 module.exports = {
     AuthRoutes : router
 }
