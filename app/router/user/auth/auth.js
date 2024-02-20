572f4ad7823a294defe20420cf71549463d27b19
amirholya01 <amirholya01@gmail.com>
2024-02-20 09:16:21 +0100

    auth router-comment

--- app/router/user/auth/auth.js
+++ app/router/user/auth/auth.js
@@ -1,12 +1,27 @@
+// Importing the AuthController from the user/auth directory within controllers
 const {AuthController} = require("../../../controllers/user/auth/Auth.Controller");
+
+// Importing the expressValidatorMapper middleware for error handling
 const {expressValidatorMapper} = require("../../../middleware/checkError");
+
+// Importing the registerValidator and loginValidator functions from the user/auth directory within validation
 const {registerValidator, loginValidator} = require("../../../validation/user/auth");
 
 
+
+// Importing the Router module from Express.js
 const router = require("express").Router();
 
+// Define routes for user authentication
+
+// Route for user registration
 router.post("/register", registerValidator(), expressValidatorMapper, AuthController.register)
+// Route for user login
 router.post("/login", loginValidator(), expressValidatorMapper, AuthController.login);
+
+
+
+// Exporting the router containing authentication routes
 module.exports = {
     AuthRoutes : router
 }
